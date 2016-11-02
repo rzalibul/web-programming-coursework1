15e5ddf9aa4da13f70ccc3665ed46b92f70a2350
Adrian Wesolowski <rzalibul95@gmail.com>
2016-11-02 12:56:19 +0000

    JavaScript changes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function clearStorage()
 /* review page functionality */
 function saveComment()
 {
-	// to add: write comments with descending date order, ratings (maybe?), adding links to all comments
+	// to add: write comments with descending date order, ratings (maybe?), adding links to all comments, XSS protection
 	var cText = $('#commentBox').val();
 	var cName = $('#nameBox').val();
 	if (cName === "")
@@ -64,7 +64,8 @@ $(".imgNav").hover	// to modify: make it a fade in to stop flicking
 (
 	function()
 	{
-		$(this).css("opacity") == "0" ? $(this).css("opacity", "1") : $(this).css("opacity", "0");	// display the navigation button when hovered and hide it when cursor stops hovering
+		$(this).css("opacity") == "0" ? $(this).css("opacity", "1") : $(this).css("opacity", "0");	
+		// display the navigation button when hovered and hide it when cursor stops hovering
 	}
 );
 
@@ -72,18 +73,43 @@ $(".imgNav").click
 (
 	function changeImage()
 	{
+		var thumbnailList = 
+		[
+			'mini1.jpg',
+			'mini2.jpg',
+			'mini3.jpg',
+			'mini4.jpg',
+			'mini5.jpg'
+		];
+		var imgList = 
+		[
+			'placeholder.png', 
+			'placeholder2.png', 
+			'placeholder3.png', 
+			'placeholder4.png', 
+			'placeholder5.png'
+		];
 		var selector = event.target;
 		if(selector.id === "prevImg")
-			changeImage.position = ++changeImage.position || 1;		// if static variable hasn't been called yet, it will be undefined and therefore initial value needs to be assigned
-		else
+			changeImage.position = --changeImage.position || -1;	// if static variable hasn't been called yet, 
+		else														// it will be undefined and therefore initial value needs to be assigned	
 		{
-			changeImage.position = --changeImage.position || -1;	// similarly to above
+			changeImage.position = ++changeImage.position || 1;		// similarly to above
 		}
 		if(changeImage.position > 0)								// display the previous/next image in this case
 		{
-			document.getElementById("fullSize").src = "some_new_relative_path";
+			document.getElementById("fullSize").src = imgList[changeImage.position];
+			if(changeImage.position % 5 == 0)						// 5 images in a slidebar - that means every fifth image is the last 
+			{														// position index starts at 0 though
+				var curThumbnails = document.getElementsByClassName("thumbnail");
+				for(var i = 0; i < 5; i++)
+				{
+					curThumbnails[i].src = thumbnailList[i + changeImage.position];
+					// subsequently replace slidebar images
+				}
+			}																	
 		}
-		else														// the slidebar could be overflown if desired; otherwise there is nothing else to do
+		else		// the slidebar could be overflown if desired; otherwise there is nothing else to do
 		{
 			
 		}	
